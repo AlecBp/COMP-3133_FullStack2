db681bf9f8dbafea3bae6b5a05704fd37a0a6855
Alec Pagliarussi <alec.paglia@gmail.com>
2021-03-09 18:58:23 -0500

    screenshots

---
+++ Lab05_101196746/README.md
@@ -0,0 +1,9 @@
+```
+node exercise-1/hello-world.js
+&&
+node exercise-2/Customer.js
+&&
+node exercise-3/Customer.js
+&&
+node exercise-4/main.js
+```
--- Lab05_101196746/exercise-4/Customer.js
+++ Lab05_101196746/exercise-4/Customer.js
@@ -2,10 +2,14 @@
 exports.__esModule = true;
 exports.Customer = void 0;
 var Customer = /** @class */ (function () {
-    function Customer(firstName, lastName) {
+    function Customer(firstName, lastName, age) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.age = age;
     }
+    Customer.prototype.getAge = function () {
+        console.log(this.firstName + " " + this.lastName + " is " + this.age + " years old");
+    };
     Customer.prototype.greeter = function () {
         console.log("Hello " + this.firstName + " " + this.lastName);
     };
